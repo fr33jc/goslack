6d9d6f6c5d87cceae5ea7d31a86e36eda26a97e6
David Phillips <david@davidphillips.email>
2014-12-13 17:31:39 -0800

    Getting the connection function all in order. Still not a proper library yet though.

--- goslack.go
+++ goslack.go
@@ -1,20 +1,16 @@
-package main
+package goslack
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 
 	"golang.org/x/net/websocket"
 )
-
-type SlackConfig struct {
-	token string
-}
 
 type SlackResponse struct {
 	Ok      bool          `json:"ok"`
@@ -29,65 +25,34 @@ type SlackMessage struct {
 	Text    string `json:"text"`
 }
 
-func NewSlackConfig() *SlackConfig {
-	return &Gopher{token: ""}
-}
-
-func Connect(config SlackConfig) ws websocket, err error {
-	resp, err := http.PostForm("https://slack.com/api/rtm.start", url.Values{"token": {g.token}})
+func ConnectSlack(token string, ws *websocket.Conn) error {
+	resp, err := http.PostForm("https://slack.com/api/rtm.start", url.Values{"token": {token}})
 	if err != nil {
-		fmt.Printf("Couldn't start realt time slack api. ERR: %v", err)
-		os.Exit(-1)
+		thisError := fmt.Sprintf("Could't start real time slack api. ERR: %v", err)
+		return errors.New(thisError)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Couldn't read response body. ERR: %v", err)
-		os.Exit(-1)
+		thisError := fmt.Sprintf("Couldn't read response body. ERR: %v", err)
+		return errors.New(thisError)
 	}
 
 	var sr SlackResponse
 	err = json.Unmarshal(body, &sr)
 	if err != nil {
-		fmt.Printf("Couldn't decode json. ERR: %v", err)
-		os.Exit(-1)
+		thisError := fmt.Sprintf("Couldn't decode json. ERR: %v", err)
+		return errors.New(thisError)
 	}
 
 	splitUrl := strings.Split(sr.Url, "/")
 	splitUrl[2] = splitUrl[2] + ":443"
 	sr.Url = strings.Join(splitUrl, "/")
 
-	ws, err := websocket.Dial(sr.Url, "", "http://localhost/")
+	ws, err = websocket.Dial(sr.Url, "", "http://localhost/")
 	if err != nil {
-		fmt.Printf("Couldn't dial websocket. ERR: %v", err)
-		os.Exit(-1)
+		thisError := fmt.Sprintf("Couldn't dial websocket. ERR: %v", err)
+		return errors.New(thisError)
 	}
-	defer ws.Close()
-}
-
-func main() {
-
-	g := NewGopher()
-	fmt.Println("Connected to slack")
-	//	msg_scanner := bufio.NewScanner(ws)
-	//var msg bytes.Buffer
-	//for msg_scanner.Scan() {
-	//		msg.WriteString(msg_scanner.Text())
-	//	}
-	//	fmt.Println(msg.String())
-	var message SlackMessage
-	message.Id += 1
-	message.Type = "message"
-	message.Channel = ""
-	message.Text = "hello slack"
-	b_message, err := json.Marshal(message)
-	if err != nil {
-		fmt.Println("Could not marshal the message. ERR: %v", err)
-		os.Exit(-1)
-	}
-	if _, err := ws.Write(b_message); err != nil {
-		fmt.Println("Couldn't write message. ERR: %v", err)
-		os.Exit(-1)
-	}
-
+	return nil
 }
